6eab19334392120972ae23a74d7cc2d78670eb68
Julien Lamarche <julien.lamarche@gmail.com>
2018-04-02 13:42:14 -0400

    first draft of volunteers model and import of csv

--- app/models/volunteer.rb
+++ app/models/volunteer.rb
@@ -1,6 +1,74 @@
 class Volunteer < ApplicationRecord
-  belongs_to :last_request
-  belongs_to :last_assignment
-  belongs_to :chapter
-  belongs_to :next_role
+    #belongs_to :last_request
+    #belongs_to :last_assignment
+    #belongs_to :chapter
+    #belongs_to :next_role
+
+    def self.import(filePath)
+        require 'csv'
+        require 'securerandom'
+
+        filePath = guessFilePath(filePath)
+
+        sep = detectSeperator(filePath)
+
+        CSV.open(filePath, 'rb', headers: :first_row, encoding: 'UTF-8', col_sep: sep) do |csv|
+            csv.each {|row|
+            }
+        end
+
+    end
+
+    class ToastmastersVolunteer
+
+        FREE_TOAST_HOST = :freeToastHost
+        TM_HQ = :ToastmastersInternational
+
+        def import(csvFile)
+            case detectCsvFileType(csvFile)
+            when Volunteer::ToastmastersVolunteer::FREE_TOAST_HOST
+            when Volunteer::ToastmastersVolunteer::TM_HQ
+            end
+        end
+
+        def self.detectCsvFileType(csvFile)
+            if csvFile.headers.include?('Customer ID')
+                return Volunteer::ToastmastersVolunteer::TM_HQ
+            elsif csvFile.headers.include?('ROLESTATUS')
+                return Volunteer::ToastmastersVolunteer::FREE_TOAST_HOST
+            end
+
+            return nil
+        end
+
+        IMPORT_MAPPING_FREE_TOAST_HOST = {
+            name: "NAME",
+            email: "EMAIL",
+            cell: "PHONE",
+            tie_breaker_uuid: SecureRandom.uuid,
+            last_synched: DateTime.now(),
+        }
+
+        IMPORT_MAPPING_TM_HQ = {
+            name: "Name",
+            email: "Email",
+            cell: "Mobile Phone",
+            tie_breaker_uuid: {
+                code:   SecureRandom.uuid,
+            },
+            last_synched: {
+                code:   DateTime.now(),
+            },
+            is_member: {
+                csvHeader: "Paid Until",
+                code:   Proc.new {|csvValue| (Date.today <= Date.parse(csvValue))}
+            },
+            external_org_id: "Customer ID",
+        }
+
+        IMPORT_PRIMARY_KEY = :email
+
+
+
+    end
 end
--- db/migrate/20180401154427_create_volunteers.rb
+++ db/migrate/20180401154427_create_volunteers.rb
@@ -6,17 +6,19 @@ class CreateVolunteers < ActiveRecord::Migration[5.1]
       t.text :cell
       t.text :tie_breaker_uuid
       t.datetime :last_synched
-      t.references :last_request, foreign_key: true
-      t.references :last_assignment, foreign_key: true
-      t.references :chapter, foreign_key: true
+      #t.references :last_request, foreign_key: true
+      #t.references :last_assignment, foreign_key: true
+      #t.references :chapter, foreign_key: true
       t.date :dont_bug_unitl
       t.boolean :dont_bug_ever
       t.text :dont_bug_ever_because
       t.boolean :is_member
-      t.references :next_role, foreign_key: true
+      t.text :external_org_id
+      #t.references :next_role, foreign_key: true
 
       t.timestamps
     end
+    add_index :volunteers, :external_org_id, unique: true
     add_index :volunteers, :cell, unique: true
     add_index :volunteers, :tie_breaker_uuid, unique: true
   end
