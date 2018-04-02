4020ad00eee4ed233747908667edce8b40e86b23
Julien Lamarche <julien.lamarche@gmail.com>
2018-04-02 13:41:17 -0400

    test for volunteers model

--- test/models/volunteer_test.rb
+++ test/models/volunteer_test.rb
@@ -1,7 +1,25 @@
 require 'test_helper'
 
 class VolunteerTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+   test "the truth" do
+     assert true
+   end
+
+   test "it should import a csv list of two csv members, sync them together" do
+        Volunteer.update_from_csv("test/files/Club-Roster20180402.csv")
+        Volunteer.update_from_csv("test/files/membership-export.csv")
+
+        member    = Volunteer.find_by_email('gkidstone2@spotify.com')
+        guest     = Volunteer.find_by_email('vwoodson34@freewebs.com')
+        #executive = Volunteer.find_by_email('bswornc@blogger.com')
+        #alumni    = Volunteer.find_by_email('tkopech@constantcontact.com')
+
+        assert member.isMember?
+        assert guest.isGuest?
+
+   end
+
+   test "it should error nicely" do
+        Volunteer.import("test/files/nonExistingFile.csv")
+   end
 end
