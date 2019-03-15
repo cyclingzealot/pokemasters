973f5f9e968613c8af9651c3a3583f6d51c6d32c
Julien Lamarche <julien.lamarche@gmail.com>
2019-03-15 16:17:30 -0400

    Added tests for request object

--- test/models/request_test.rb
+++ test/models/request_test.rb
@@ -1,7 +1,25 @@
 require 'test_helper'
 
 class RequestTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "the truth" do
+     assert true
+  end
+
+  def self.populateDb()
+      Role::update_from_csv('test/files/roles.csv')
+
+  end
+
+  test "It can determine the next volunteer for a role (more volunteers then roles)" do
+=begin
+    What is a good scenario here?  How to mituclouisy test 'oh you got the right role'?  Well, there are two possible state:
+    1. We have an equal amount of roles and volunteers, or fewer volunteers then roles
+    2. We have more volunteers then roles
+=end
+
+  end
+
+
+
+
 end
