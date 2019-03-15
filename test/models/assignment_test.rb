4406b2f6e220a2e37ee3199fd7c963509256f0f7
Julien Lamarche <julien.lamarche@gmail.com>
2019-03-15 11:59:54 -0400

    Added assign meeting to volunteer

--- test/models/assignment_test.rb
+++ test/models/assignment_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class AssignmentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "the truth" do
+    assert true
+  end
+
+  test "Assign a volunteer to a meeting" do
+    VolunteerTest::populateDb()
+
+    v1 = Volunteer.find_by_email('gkidstone2@spotify.com')
+
+    assert_not_nil v1
+
+    m = Meeting.new("2019/03/15 18:55", "Robertston 617")
+    r = Role.snameLike('ge').first
+
+    m.assign(v1, r)
+    #a = Assignment.new(volunteer: v1, meeting: m, role: r)
+  end
 end
