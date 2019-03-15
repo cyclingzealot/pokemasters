9cceff8cc3335377e47d922437baace5e430931e
Julien Lamarche <julien.lamarche@gmail.com>
2019-03-15 12:00:12 -0400

    Added initize test for meeting

--- test/models/meeting_test.rb
+++ test/models/meeting_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class MeetingTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+
+    test "I can create myself" do
+        m = Meeting.new("2019/03/18 18:35", "Robertston 617")
+
+        assert_not_nil m
+
+        #m.save!
+    end
+
+
+
 end
