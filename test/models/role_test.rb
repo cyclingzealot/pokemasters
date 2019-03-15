84c751ec8119cbe54d696176d7d3355d9efc8677
Julien Lamarche <julien.lamarche@gmail.com>
2019-03-15 16:17:49 -0400

    Added tests for role model, namely laod from csv

--- test/models/role_test.rb
+++ test/models/role_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class RoleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "the truth" do
+    assert true
+  end
+
+  def self.populateDb()
+    Role::update_from_csv('test/files/roles.csv')
+  end
+
+
+  test "It can load a csv file" do
+    self.class.populateDb
+    assert_operator Role.count, :>, 0
+  end
 end
