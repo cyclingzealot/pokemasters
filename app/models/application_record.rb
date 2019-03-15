fb1777a4b99b36400b860d1490f7a9a3cd576233
Julien Lamarche <julien.lamarche@gmail.com>
2019-03-15 16:16:42 -0400

    Added random method to active record objects

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,22 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+
+  def self.random(amount = nil)
+    stuff = self.where('1=1').order("RANDOM()")
+
+    return stuff.take if amount.nil?
+    return stuff.limit(amount) if amount != :all
+    return stuff
+  end
+
+
+    def load_from_csv(model = self, filepath)
+      CSV.open filepath, 'rb', headers: :first_row, encoding: 'UTF-8' do |csv|
+        csv.each do |row|
+          model.create! row.to_hash
+        end
+      end
+    end
+
 end
