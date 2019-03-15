f99f32c7fddbdb8f037c4e0944912f9381356b73
Julien Lamarche <julien.lamarche@gmail.com>
2019-03-15 11:59:29 -0400

    Added creating for meeting

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -1,9 +1,17 @@
 class Meeting < ApplicationRecord
 
+    attr_reader :datetime
+    attr_reader :location
 
     def assign(volunteer, role)
         a = Assignment.new(volunteer: volunteer, role: role, meeting: self)
-        a.save!
+        #a.save
+        return a
     end
-        
+
+    def initialize(datetime, where)
+        @datetime = DateTime.parse(datetime)
+        @location = where
+    end
+
 end
