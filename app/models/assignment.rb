b3f8812243f8a548ccabe14b9e581b377320a40a
Julien Lamarche <julien.lamarche@gmail.com>
2019-03-15 11:59:20 -0400

    Added creator for assignment

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -2,4 +2,11 @@ class Assignment < ApplicationRecord
   belongs_to :volunteer
   belongs_to :meeting
   belongs_to :role
+
+
+  def initialize(volunteer:, meeting:, role:)
+    @volunteer = volunteer
+    @meeting   = meeting
+    @role      = role
+  end
 end
