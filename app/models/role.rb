e0fd37823db511273e5165b489e4e637ed4e84f4
Julien Lamarche <julien.lamarche@gmail.com>
2019-03-15 11:59:41 -0400

    Added snameLike for Roel

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,2 +1,9 @@
 class Role < ApplicationRecord
+
+    def self.snameLike(searchStr)
+        likeStr = "like"
+        likeStr = "ilike" if ActiveRecord::Base.connection.instance_values["config"][:adapter] == "postgresql"
+        where(%Q[short_name #{likeStr} '%#{searchStr}%'])
+    end
+
 end
