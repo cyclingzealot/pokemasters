0a0cbb55a9a1d21dac368e93b8bd59b9f41cb81a
Julien Lamarche <julien.lamarche@gmail.com>
2019-03-15 16:16:59 -0400

    Added UpdatableFromCsv to Role

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,9 +1,12 @@
 class Role < ApplicationRecord
+    extend UpdatableFromCsv, ModelHelper
 
     def self.snameLike(searchStr)
         likeStr = "like"
         likeStr = "ilike" if ActiveRecord::Base.connection.instance_values["config"][:adapter] == "postgresql"
         where(%Q[short_name #{likeStr} '%#{searchStr}%'])
     end
+
+
 
 end
