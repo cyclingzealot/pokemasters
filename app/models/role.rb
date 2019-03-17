27f98e3579d37ced0fba81b7bd0664d040e9e0c7
Julien Lamarche <julien.lamarche@gmail.com>
2019-03-17 16:29:45 -0400

    Added sNameLike, find_by_sname, suggestVolunteer methods to Role

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,10 +1,26 @@
 class Role < ApplicationRecord
     extend UpdatableFromCsv, ModelHelper
 
+    validates :short_name, :human_name, presence: true
+
     def self.snameLike(searchStr)
+        sNameLike(searchStr)
+    end
+
+    def self.sNameLike(searchStr)
         likeStr = "like"
         likeStr = "ilike" if ActiveRecord::Base.connection.instance_values["config"][:adapter] == "postgresql"
         where(%Q[short_name #{likeStr} '%#{searchStr}%'])
+    end
+
+    def self.find_by_sname(searchStr)
+        find_by_short_name(searchStr)
+    end
+
+    def suggestVolunteer
+        #Always find someone eligeable who has not done the role
+        #If no one, rank by last done.
+
     end
 
 
