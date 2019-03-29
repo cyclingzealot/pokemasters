b32ae978d47cbeca0759d526d4df5650a2f941a2
Julien Lamarche <julien.lamarche@gmail.com>
2019-03-29 13:28:47 -0400

    adding more specific test cases for role

--- test/models/role_test.rb
+++ test/models/role_test.rb
@@ -15,4 +15,17 @@ class RoleTest < ActiveSupport::TestCase
     assert_operator Role.count, :>, 10
   end
 
+
+  test "It will suggest only brand new volunteers" do
+    Role.find_by_sname('timer').suggestVolunteers.each {|r| assert_equal r.assignments.count, 0, "This volunteer had previous assinments"}
+  end
+
+
+  test "Once there are no volunteers to pick from, pick someone who has done the furthest away who has no role in the meetings in the next 2 weeks" do
+    Role.find_by_sname('timer').suggestVolunteers.each {|r| test_skip }
+  end
+
+
+
+
 end
