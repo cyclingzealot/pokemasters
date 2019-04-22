4de46192774c9e1fe90ad2c2991135420c8a67de
Julien Lamarche <julien.lamarche@gmail.com>
2019-04-21 22:23:26 -0400

    Registration with an organization

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -3,10 +3,22 @@ class Assignment < ApplicationRecord
   belongs_to :meeting
   belongs_to :role
 
+  validate :has_registration_with_org
+
 
   def initialize(volunteer:, meeting:, role:)
     @volunteer = volunteer
     @meeting   = meeting
     @role      = role
   end
+
+
+    private
+
+    def has_registration_with_org
+        if not self.volunteer.isRegisteredTo?(organization: self.meeting.organization)
+			errors.add(:meeting, "#{self.volunteer.to_s} is not registered to #{self.meeting.organization.to_s}")
+		end
+    end
+
 end
